664e8fe6ea1923ecc945299b53ea8d23cfd78b99
Robbie Carlton <robbie.carlton@gmail.com>
2020-06-17 08:56:19 -0600

    initial commit

---
+++ precache-manifest.42579a7937dfe4fa2a91940b09f88783.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c064f099ea1bc7d4ea51229972e3a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64fb13ef19b4dae4fa42",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3a98b18484ac687725bf",
+    "url": "/static/js/2.d8428b5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d8428b5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64fb13ef19b4dae4fa42",
+    "url": "/static/js/main.73c11027.chunk.js"
+  },
+  {
+    "revision": "bad2a1cd62f11641430e",
+    "url": "/static/js/runtime-main.fc297e79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
